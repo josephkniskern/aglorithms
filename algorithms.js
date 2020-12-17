c05654ea7ed7aa0b21670c81b9c13f19212a53e3
Joseph Kniskern <kniskernjoseph@yahoo.com>
2020-12-16 19:58:35 -0500

    more algos

--- algorithms.js
+++ algorithms.js
@@ -99,3 +99,20 @@ function sumAll(arr) {
 }
 
 sumAll([1, 4]);
+
+/////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////
+
+
+//*Compare two arrays and return a new array with any items 
+//*only found in one of the two given arrays, but not both. 
+//*In other words, return the symmetric difference of the two arrays.
+
+function diffArray(arr1, arr2) {
+  return arr1
+    .concat(arr2)
+    .filter(n => (!arr1.includes(n) || !arr2.includes(n)))
+}
+
+diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
