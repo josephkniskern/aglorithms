7e4664b861647638144cabc4a35caa9551afd649
Joseph Kniskern <kniskernjoseph@yahoo.com>
2020-12-23 22:14:13 -0500

    algos

--- algorithms.js
+++ algorithms.js
@@ -156,3 +156,22 @@ function translatePigLatin(str) {
 }
 
 translatePigLatin("consonant");
+
+/////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////
+
+//*Write a function that takes two or more arrays
+//*and returns a new array of unique values in the order of the original provided arrays.
+//*In other words, all values present from all arrays s
+//*hould be included in their original order, but with no duplicates in the final array.
+//*The unique numbers should be sorted by their original order,
+//*but the final array should not be sorted in numerical order.
+
+function uniteUnique(arr) {
+  return [...new Set(arr.flat())];
+}
+
+uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
+
+//! Set object in JS
