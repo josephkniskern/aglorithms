08f3a6b468a34f1f4cd57152d6c71fc83e748979
Joseph Kniskern <kniskernjoseph@yahoo.com>
2020-12-30 20:24:31 -0500

    algos

--- algorithms.js
+++ algorithms.js
@@ -228,3 +228,25 @@ function multiplesOf3and5(number) {
   }
   return sum;
 }
+
+/////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////
+
+const fiboEvenSum = (number) => {
+  if (number <= 1) {
+    return 0;
+  } else {
+    let evenSum = 0,
+      prevFibNum = 1,
+      // According to problem description our Fibonacci series starts with 1, 2
+      fibNum = 2;
+    for (let i = 2; fibNum <= number; i++) {
+      if (fibNum % 2 == 0) {
+        evenSum += fibNum;
+      }
+      [prevFibNum, fibNum] = [fibNum, prevFibNum + fibNum];
+    }
+    return evenSum;
+  }
+};
