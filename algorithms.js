9f0d8a5cac71338efd8c3fad4c046a2c94d13e1e
Joseph Kniskern <kniskernjoseph@yahoo.com>
2020-12-30 20:27:13 -0500

    algos

--- algorithms.js
+++ algorithms.js
@@ -262,6 +262,10 @@ const fiboEvenSum = (number) => {
 /////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////
 
+//*The prime factors of 13195 are 5, 7, 13 and 29.
+
+//*What is the largest prime factor of the given number?
+
 function largestPrimeFactor(number) {
   let prime = 2,
     max = 1;
@@ -269,7 +273,7 @@ function largestPrimeFactor(number) {
     if (number % prime == 0) {
       max = prime;
       number = number / prime;
-    } else prime++; //Only increment the prime number if the number isn't divisible by it
+    } else prime++; //!Only increment the prime number if the number isn't divisible by it
   }
   return max;
 }
