cc8a36a3805a0cfa81220bba0c062969f36f0f41
Joseph Kniskern <kniskernjoseph@yahoo.com>
2021-01-06 21:28:25 -0500

    algos

--- algorithms.js
+++ algorithms.js
@@ -294,3 +294,13 @@ const compareTriplets = (a, b) => {
   }
   return points;
 };
+
+function bubbleSort(array) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length - 1 - i; j++) {
+      if (array[j] > array[j + 1])
+        [array[j], array[j + 1]] = [array[j + 1], array[j]]; // Using ES6 array destructuring to swap
+    }
+  }
+  return array;
+}
