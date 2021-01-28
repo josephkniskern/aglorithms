d13a5a2dd39ddcb91cb007f24b8a22fd7795ee88
Joseph Kniskern <kniskernjoseph@yahoo.com>
2021-01-27 20:37:10 -0500

    algos

--- other.js
+++ other.js
@@ -88,10 +88,7 @@ function commonPrimeFactors(num1, num2) {
   return primeFactors;
 }
 
-
 ////////////////////////////////////////////
-
-
 
 /***********************************************************************
 Write a function splitHalfArray(array) that takes in an array as an
@@ -123,5 +120,37 @@ function splitHalfArray(array) {
 function splitHalfArray(array) {
   var midIndex = Math.floor(array.length / 2);
 
-  return [array.slice(0, midIndex), array.slice(midIndex + array.length % 2)];
+  return [array.slice(0, midIndex), array.slice(midIndex + (array.length % 2))];
+}
+
+////////////////////////////////////////
+
+/***********************************************************************
+Write a function threeUniqueVowels(string) that takes in a string and
+returns true if the string contains at least three different vowels.
+
+Vowels are a, e, i, o, u
+
+Examples:
+
+threeUniqueVowels('delicious'); // => true
+threeUniqueVowels('bootcamp prep'); // => true
+threeUniqueVowels('bootcamp'); // => false
+threeUniqueVowels('dog'); // => false
+threeUniqueVowels('go home'); // => false
+***********************************************************************/
+
+function threeUniqueVowels(string) {
+  var vowels = ["a", "e", "i", "o", "u"];
+  var count = 0;
+
+  for (var i = 0; i < vowels.length; i += 1) {
+    var vowel = vowels[i];
+
+    if (string.indexOf(vowel) > -1) {
+      count += 1;
+    }
+  }
+
+  return count >= 3;
 }
